629d35a98a12536e59217e8ca54b45bdec12355c
Sam Masto <56971406+smashed-toes@users.noreply.github.com>
2025-10-17 18:40:29 -0700

    Project card updates

--- smashed-toes-acting/src/components/ProjectCard.tsx
+++ smashed-toes-acting/src/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 interface ProjectCardProps {
   image: string;
@@ -8,15 +8,46 @@ interface ProjectCardProps {
 }
 
 function ProjectCard({ image, title, description, url }: ProjectCardProps) {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  // Mobile version of project card
+  if (isMobile) {
+    console.log("Mobile");
+    return (
+      <div className="flex flex-col ">
+        {url ? (
+          <>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              <img src={image} alt={title} className="w-full h-auto" />
+            </a>
+            <p className="italic text-center mt-2">
+              Click the image above to watch
+            </p>
+          </>
+        ) : (
+          <img src={image} alt={title} className="w-full h-auto" />
+        )}
+        <div className="inset-0 flex items-center justify-center text-white">
+          <div className="p-4">
+            <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+            <p>{description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // Desktop version of project card
   return (
     <div className="relative group">
       {url ? (
         <a href={url} target="_blank" rel="noopener noreferrer">
           <img src={image} alt={title} className="w-full h-auto" />
           <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-80 flex items-center justify-center text-white">
-            <div className="p-4">
-              <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+            <div className="p-4 space-y-2">
+              <h2 className="text-2xl font-semibold">{title}</h2>
               <p>{description}</p>
+              <p className="italic">Click to watch</p>
             </div>
           </div>
         </a>
