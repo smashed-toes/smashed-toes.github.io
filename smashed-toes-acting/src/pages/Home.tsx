ee9a483ca1b97819a9d33124f6aa9c5bf71e119e
Sam Masto <56971406+smashed-toes@users.noreply.github.com>
2025-10-17 17:30:57 -0700

    vimeo loading animation

--- smashed-toes-acting/src/pages/Home.tsx
+++ smashed-toes-acting/src/pages/Home.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleVideoLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className="flex flex-col px-8 justify-center items-center space-y-4">
       <h1 className="text-4xl font-bold">Hey now</h1>
@@ -6,11 +14,17 @@ function Home() {
         I'm guessing you came here for my reel, but if you want to see more of
         my work, click on the links in the navigation bar above.
       </p>
-      <div className="flex w-[90%] h-full aspect-video">
+      <div className="relative w-[90%] h-full aspect-video">
+        {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-purple-500 animate-pulse z-10">
+            <p className="font-display text-white text-5xl">Loading Reel...</p>
+          </div>
+        )}
         <iframe
-          className="aspect-video"
+          className="w-full h-full"
           src="https://player.vimeo.com/video/882724629?h=f340a06f6d"
-          title="YouTube video player"
+          title="Vimeo video player"
+          onLoad={handleVideoLoad}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
