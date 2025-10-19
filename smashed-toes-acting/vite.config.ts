3e1294a62a97a5fe01f4c4ec5fc5906a4f057187
Sam Masto <56971406+smashed-toes@users.noreply.github.com>
2025-10-18 20:43:01 -0700

    no more redirecting error

--- smashed-toes-acting/vite.config.ts
+++ smashed-toes-acting/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { copyFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // Ensures user can reload page at current path.
+    {
+      name: "copy-404",
+      closeBundle() {
+        copyFileSync("dist/index.html", "dist/404.html");
+      },
+    },
+  ],
   base: "/",
 });
