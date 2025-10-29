d399b1d7c999017c86f8a9113bc6f589a4ab9f03
Sam Masto <56971406+smashed-toes@users.noreply.github.com>
2025-10-28 22:25:24 -0700

    no more third person

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,27 +16,24 @@ function About() {
   return (
     <div className="p-8 flex flex-col ">
       <h1 className="text-4xl font-bold font-heading">About Me</h1>
-      <h2 className="text-xl font-bold font-heading mt-2">
-        I know these are usually written in 3rd person, but I wrote this so I'm
-        using 1st person. Sue me.
-      </h2>
       <div className="flex flex-col-reverse md:flex-row">
         <div className="w-full md:w-2/3 pr-5">
           <p className="mt-4 text-2xl">
-            I'm an actor, singer, and improviser based in Los Angeles.
-            Originally from San Francisco, I studied at Brown University where I
-            split my time between Theater and Computer Science. In 2021, I was
-            awarded the MH Hicks Award for outstanding work in Brown's Theater
-            Arts and Performance Studies department.
-            <br /> <br /> After graduating, I moved to LA with the goal of
-            wearing a t-shirt in January. Since then, I've performed in a
+            Sam Masto is your quintessential triple threat (actor, singer,
+            computer scientist). Originally from San Francisco, he studied at
+            Brown University where he split his time between Theater and
+            Computer Science. In 2021, he was awarded the MH Hicks Award for
+            outstanding work in Brown's Theater Arts and Performance Studies
+            department.
+            <br /> <br /> After graduating, he moved to LA with the goal of
+            wearing a t-shirt in January. Since then, he's performed in a
             variety of award-winning short films (Work Life, Apologized), the
             2024 Feature Film "Gnome Sick: 7 Slays Till Mithras", theatrical
             productions (Cannibal: The Musical, Space Captain: Captain of
-            Space), and various improv and sketch shows. <br /> <br /> I'm
-            currently working through the Groundlings lab track and I
-            additionally train at UCB. I'm commercially represented by AKA
-            Talent Agency. In my free time, you can find me head-banging at a
+            Space), and various improv and sketch shows. <br /> <br /> He's
+            currently working through the Groundlings lab track and he
+            additionally trains at UCB. He is commercially represented by AKA
+            Talent Agency. In his free time, you can find him head-banging at a
             concert, running a D&D session, or maybe singing about drinking piña
             coladas in the rain.
           </p>
